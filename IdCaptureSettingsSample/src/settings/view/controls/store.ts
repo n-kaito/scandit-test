f4dba0de17e43173ae73456d84210c420d4482de
kaito <nkaitou@gmail.com>
2024-05-31 10:53:27 +0900

    first commit

--- IdCaptureSettingsSample/src/settings/view/controls/store.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store";
-
-export const torchSwitchControlEnabled = writable(false);
-export const cameraSwitchControlEnabled = writable(false);
-export const allowPictureInPicture = writable(true);
